5a63924ccf14823b1ffd1d639bf68c6b1f266edd
Jacinto Mba Cantero <jacmba@gmail.com>
2021-04-26 01:53:23 +0100

    Added submarine to title screen

--- Assets/Scripts/Misc/Rotate.cs
+++ Assets/Scripts/Misc/Rotate.cs
@@ -7,11 +7,14 @@ public class Rotate : MonoBehaviour
   [SerializeField]
   private float speed = 15.0f;
 
+  [SerializeField]
+  private Vector3 axis = Vector3.up;
+
   /// <summary>
   /// Update is called once per frame
   /// </summary>
   void Update()
   {
-    transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    transform.Rotate(axis * speed * Time.deltaTime);
   }
 }
