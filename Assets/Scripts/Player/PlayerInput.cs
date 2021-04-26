a0f4f01e4d6d2f4967055ca62f75d9e114bf6377
Jacinto Mba Cantero <jacmba@gmail.com>
2021-04-26 16:52:21 +0100

    Aim and player orientation

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -7,11 +7,17 @@ public class PlayerInput : MonoBehaviour
   [SerializeField]
   private float speed = 5f;
 
+  [SerializeField]
+  private Transform aim;
+
+  private float aimDist;
+
   /// <summary>
   /// Start is called before the first frame update
   /// </summary>
   void Start()
   {
+    aimDist = aim.position.z - transform.position.z;
   }
 
   /// <summary>
@@ -24,5 +30,27 @@ public class PlayerInput : MonoBehaviour
 
     Vector3 translation = new Vector3(h, v, 0) * speed * Time.deltaTime;
     transform.Translate(translation, Space.World);
+    transform.LookAt(aim.position, Vector3.down);
+    transform.Rotate(Vector3.right * 90f);
+  }
+
+  /// <summary>
+  /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void FixedUpdate()
+  {
+    Vector3 point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, aimDist);
+    Vector3 lookPos = Camera.main.ScreenToWorldPoint(point);
+
+    RaycastHit hit;
+    bool touched = Physics.Raycast(transform.position, lookPos - transform.position, out hit, 200f);
+    if (touched)
+    {
+      aim.position = hit.point;
+    }
+    else
+    {
+      aim.position = new Vector3(lookPos.x, lookPos.y, transform.position.z + aimDist);
+    }
   }
 }
