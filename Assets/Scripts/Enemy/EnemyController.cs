96d95744b8d055f29c69be114aad0a434e3652fa
Jacinto Mba Cantero <jacmba@gmail.com>
2021-05-02 21:19:52 +0100

    :construction: Enemy behavior

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+  [SerializeField]
+  private EnemyType type;
+
+  [SerializeField]
+  private float swimSpeed;
+
+  [SerializeField]
+  private float stareDist;
+
+  [SerializeField]
+  private float attackDist;
+
+  /// <summary>
+  /// Start is called before the first frame update
+  /// </summary>
+  void Start()
+  {
+  }
+
+  /// <summary>
+  /// Update is called once per frame
+  /// </summary>
+  void Update()
+  {
+  }
+}
